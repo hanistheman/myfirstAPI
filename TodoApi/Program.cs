f2156d3f45cc1563140aa6336705b1133b32fcf8
hanistheman <gao647@purdue.edu>
2024-09-22 13:41:37 -0400

    Added code to the API to make it functional

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,6 +1,81 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using NSwag.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb> (opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+// Enables API explorer to provide metadata about this API
+builder.Services.AddOpenApiDocument(config => 
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
+// Adds the Swagger OpenAPI document generator
+var app = builder.Build();
+if (app.Environment.IsDevelopment()) 
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config => 
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+    /* Enables Swagger middleware for serving the generated JSON document 
+    and the Swagger UI
+    */
+}
+var todoItems = app.MapGroup ("/todoitems");
+
+todoItems.MapGet("/", async (TodoDb db) => 
+    await db.Todos.ToListAsync());
+
+todoItems.MapGet("/complete", async (TodoDb db) => 
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+todoItems.MapGet("/{id}", async (int id, TodoDb db) => 
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+todoItems.MapPost("/", async (Todo todo, TodoDb db) => 
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDb db) => 
+{
+    var todo = await db.Todos.FindAsync(id);
+    
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+todoItems.MapDelete("/{id}", async (int id, TodoDb db) => 
+{
+    if (await db.Todos.FindAsync(id) is Todo todo) 
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f68fbedac2dc02105a4a0019dcc2a3affd45b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50506b723d70668cd4b92a75e075e93139a73b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
